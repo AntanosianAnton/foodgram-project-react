8f186afae8575e031290aeb27a54597860782254
Anton <anton,antanosyan@gmail.com>
2022-11-13 17:32:31 +0300

    fix ingredient  validation

--- backend/foodgram/recipe/serializers.py
+++ backend/foodgram/recipe/serializers.py
@@ -124,7 +124,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ('id', 'author', 'name', 'image', 'text',
                   'ingredients', 'tags', 'cooking_time',)
 
-    def validate_(self, data):
+    def validate_ingredients(self, data):
         ingredients = self.initial_data.get('ingredients')
         if not ingredients:
             raise ValidationError(
